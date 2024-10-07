7bd984956e19f75e3a981932f8998827ee8066f9
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-10-07 21:38:47 +0700

    update CRUD for Album

---
+++ src/main/java/com/example/daydreamer/model/album/AlbumRequest.java
@@ -0,0 +1,35 @@
+package com.example.daydreamer.model.album;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlbumRequest {
+
+    private String id;
+
+    @NotNull(message = "Studio ID cannot be null")
+    @NotBlank(message = "Studio ID cannot be blank")
+    private String studioId;
+
+    private List<String> albumPhotoIds;
+
+    @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotNull(message = "Price cannot be null")
+    private Double price;
+
+    @NotNull(message = "Status cannot be null")
+    @NotBlank(message = "Status cannot be blank")
+    private String status;
+}
