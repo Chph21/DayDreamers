3681134038fcaa83ff4e206e325535934c20e9ff
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-10-13 17:56:42 +0700

    Update CRUD for Studio, change generate response logic

--- src/main/java/com/example/daydreamer/repository/StudioRepository.java
+++ src/main/java/com/example/daydreamer/repository/StudioRepository.java
@@ -2,8 +2,9 @@ package com.example.daydreamer.repository;
 
 import com.example.daydreamer.entity.Studio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudioRepository extends JpaRepository<Studio, String> {
+public interface StudioRepository extends JpaRepository<Studio, String>, JpaSpecificationExecutor<Studio> {
 }
