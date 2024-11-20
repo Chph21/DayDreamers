3d5b3d83f0d5d390a5d1245878a4fe937c58cc2a
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-11-20 23:40:01 +0700

    fix verify account

--- src/main/java/com/example/daydreamer/service/EmailService.java
+++ src/main/java/com/example/daydreamer/service/EmailService.java
@@ -4,7 +4,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -38,29 +37,29 @@ public class EmailService {
         javaMailSender.send(message);
     }
 
-    @Async
-    public void sendWelcomeEmail(String userEmail, String name, String password) throws MessagingException {
-        String subject = "Welcome to Our Website!";
-        String body = "<html><body style='font-family: Arial, sans-serif;'>"
-                + "<h2 style='color: #007bff;'>Welcome to Our Website, " + name + "!</h2>"
-                + "<p>We are thrilled to welcome you to our website!</p>"
-                + "<p>Your account has been successfully created. Below are your login credentials:</p>"
-                + "<ul>"
-                + "<li><strong>Username:</strong> " + userEmail + "</li>"
-                + "<li><strong>Password:</strong> " + password + "</li>"
-                + "</ul>"
-                + "<p>Please keep your login credentials secure and do not share them with anyone.</p>"
-                + "<p>Thank you for choosing our services. We hope you enjoy your experience on our website!</p>"
-                + "<p style='color: #28a745;'>Best regards,<br/>Your Website Team</p>"
-                + "</body></html>";
-
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-        helper.setTo(userEmail);
-        helper.setSubject(subject);
-        helper.setText(body, true);
-        javaMailSender.send(message);
-    }
+//    @Async
+//    public void sendWelcomeEmail(String userEmail, String name, String password) throws MessagingException {
+//        String subject = "Welcome to Our Website!";
+//        String body = "<html><body style='font-family: Arial, sans-serif;'>"
+//                + "<h2 style='color: #007bff;'>Welcome to Our Website, " + name + "!</h2>"
+//                + "<p>We are thrilled to welcome you to our website!</p>"
+//                + "<p>Your account has been successfully created. Below are your login credentials:</p>"
+//                + "<ul>"
+//                + "<li><strong>Username:</strong> " + userEmail + "</li>"
+//                + "<li><strong>Password:</strong> " + password + "</li>"
+//                + "</ul>"
+//                + "<p>Please keep your login credentials secure and do not share them with anyone.</p>"
+//                + "<p>Thank you for choosing our services. We hope you enjoy your experience on our website!</p>"
+//                + "<p style='color: #28a745;'>Best regards,<br/>Your Website Team</p>"
+//                + "</body></html>";
+//
+//        MimeMessage message = javaMailSender.createMimeMessage();
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+//        helper.setTo(userEmail);
+//        helper.setSubject(subject);
+//        helper.setText(body, true);
+//        javaMailSender.send(message);
+//    }
 
 
 }
